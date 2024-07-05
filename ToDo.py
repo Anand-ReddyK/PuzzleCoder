5b25dfa861e809b3b7fa586bfc5e60ff50fb77d0
Anand-ReddyK <karraanand5@gmail.com>
2024-07-05 22:59:28 +0530

    Added a TODO list file

---
+++ ToDo.py
@@ -0,0 +1,5 @@
+"""
+*Create a Inttialize DB or Migrate Db file to create the necessary indexes
+TODO: Create a form that takes the structure of the problem
+TODO: Add a difficulty field to the problems
+"""
