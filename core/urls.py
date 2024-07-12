ea4840bbd30b75e2ba357f6cd21ee68ccf696494
Anand-ReddyK <karraanand5@gmail.com>
2024-07-12 21:08:12 +0530

    changed the problem_detail layout

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.urls import  path
 from . import views
 
 urlpatterns = [
-    path('problems/', views.problems_page, name="problems_page"),
+    path('', views.problems_page, name="problems_page"),
     path('add/', views.add_problem, name="add_problem"),
     path("problem/<int:id>/", views.problem_detail, name="problem_detail"),
     path("login/", views.login_view, name="login"),
